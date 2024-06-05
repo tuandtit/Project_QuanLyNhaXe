29622dfb42a93a4dbd314820d3188c1cfe4018ed
Tuandtit <tuandtit6326@gmail.com>
2024-06-05 14:58:43 +0700

    Them package

--- src/main/java/group7_java/showroom_auto88_management/Group7_JavaShowroom_Auto88_Management.java
+++ src/main/java/group7_java/showroom_auto88_management/Group7_JavaShowroom_Auto88_Management.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.group7_java.showroom_auto88_management;
+package group7_java.showroom_auto88_management;
 
 /**
  *
