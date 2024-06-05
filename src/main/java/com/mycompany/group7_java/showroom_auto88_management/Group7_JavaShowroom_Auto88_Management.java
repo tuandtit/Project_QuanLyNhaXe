1449ad45b220d5f475f0f0fc4b14f8b880c9860c
Tuandtit <tuandtit6326@gmail.com>
2024-06-05 14:49:40 +0700

    Khoi tao du an

---
+++ src/main/java/com/mycompany/group7_java/showroom_auto88_management/Group7_JavaShowroom_Auto88_Management.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.group7_java.showroom_auto88_management;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Group7_JavaShowroom_Auto88_Management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
